8b3e09e4534007f01803ae6ae8e2ef51c36b490f
Alex <alexander.cardenas@globant.com>
2022-02-24 20:41:28 -0500

    Challenge topic_4

---
+++ src/Challenges/Topic_4/docker/dump.sql
@@ -0,0 +1,21 @@
+create table docker_academy_db.USER (
+    username varchar(255),
+    name varchar(255),
+    last_name varchar(255),
+    identification_number long,
+    address varchar(255),
+    zip_code_city varchar(255),
+    state varchar(255),
+    country varchar(255)
+);
+
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('igascone0', 'Iggy', 'Gascone', '6378585115', '62620 Old Shore Street', '110915', 'Yutan', 'China');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('jhabbijam1', 'Joan', 'Habbijam', '5275793325', '746 Declaration Trail', '110640', 'Johor Bahru', 'Malaysia');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('sledner2', 'Sybille', 'Ledner', '0647892403', '35 Scoville Parkway', '110152', 'Wanmao', 'China');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('cdebeauchemp3', 'Carlyn', 'De Beauchemp', '6799282820', '10 Welch Parkway', '110786', 'As Sālimīyah', 'Kuwait');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('krhelton4', 'Kris', 'Rhelton', '6208791017', '5 Donald Point', '110476', 'Soloma', 'Guatemala');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('apearlman5', 'Amil', 'Pearlman', '8407711455', '2 Randy Park', '110780', 'Vân Đình', 'Vietnam');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('hlarge6', 'Hugo', 'Large', '3589758281', '8299 Gerald Center', '110316', 'Krylovskaya', 'Russia');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('pknewstubb7', 'Prentice', 'Knewstubb', '7939277129', '83 Everett Crossing', '110307', 'La Eduvigis', 'Argentina');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('gmaylam8', 'Gretchen', 'Maylam', '1187376352', '9092 Spenser Terrace', '110993', 'Imtarfa', 'Malta');
+insert into docker_academy_db.USER (username, name, last_name, identification_number, address, zip_code_city, state, country) values ('fitzhayek9', 'Fanechka', 'Itzhayek', '7759386748', '13209 Marquette Avenue', '110792', 'Daxing', 'China');
